7b2b7d39d47ada0ec43c4d90e432b3dbd7bb2904
patel-zeel <40241204+rishi-a@users.noreply.github.com>
2022-02-28 18:30:43 +0530

    Update CCCS811_SD_Card_Interface.ino

--- Arduino_Codes/CCCS811_SD_Card_Interface/CCCS811_SD_Card_Interface.ino
+++ Arduino_Codes/CCCS811_SD_Card_Interface/CCCS811_SD_Card_Interface.ino
@@ -58,7 +58,7 @@ void loop() {
   }
   // open the file. note that only one file can be open at a time,
   // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
+  File dataFile = SD.open("datalog4.txt", FILE_WRITE);
 
   // if the file is available, write to it:
   if (dataFile) {
@@ -74,5 +74,5 @@ void loop() {
     Serial.println("error opening datalog.txt");
     digitalWrite(writeIndicator, LOW);
   }
-  delay(1000);
+  delay(500);
 }
